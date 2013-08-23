49e07811f61d9612dadc6e2b07f0323a562dbef3
Vitaly Antonenko <vantonenko@arccn.ru>
2013-08-23 16:00:06 +0400

    Add Graph Editor

--- GUI/build/graph-editor.js
+++ GUI/build/graph-editor.js
@@ -75,7 +75,7 @@ function circle(x,y,r,nofillFlag){
 }
 
 function line(x1,y1,x2,y2){
-    ctx.lineWidth = 1;
+    ctx.lineWidth = 2;
     ctx.beginPath();
     ctx.moveTo(x1,y1);
     ctx.lineTo(x2,y2);
