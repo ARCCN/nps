83955e593599c054d6f0ccbbdc0824a84aadaa73
Vitaly Antonenko <vantonenko@arccn.ru>
2013-12-05 13:33:43 +0400

    small fix

--- main.py
+++ main.py
@@ -132,6 +132,10 @@ if __name__ == '__main__':
     mininet_script_generator.generate_mininet_turn_on_script_auto(node_intf_map, node_groups,
                                                                       edge_groups, node_ext_intf_group, leaves,
                                                                       node_map, node_ctrl_map)
+    # USE WITH NSP with net apps
+    #mininet_script_generator.generate_mn_ns_script_auto(node_intf_map, node_groups,
+    #                                                                  edge_groups, node_ext_intf_group, leaves,
+    #                                                                  node_map, node_ctrl_map, node_services)
     print('DONE!')
 
     # send scripts to nodes
